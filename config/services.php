8b0517c237668358bbabdee8cf7352bd0ae93fcc
viviet <viviet@tutanota.com>
2020-05-28 03:55:24 +0900

    fix

--- config/services.php
+++ config/services.php
@@ -43,8 +43,8 @@ return [
     ],
 
     'github' => [
-        'client_id' => env('GITHUB_KEY'),
+        'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_SECRET'),
-        'redirect' => env('GITHUB_REDIRECT_URI')
+        'redirect' => env('GITHUB_REDIRECT_URL')
     ],
 ];
